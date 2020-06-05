fe2fbca66db063efffaf64d5d303b9c0b6f2f34a
Lucas Sato <lucasjks@gmail.com>
2020-06-04 21:39:44 -0300

    Updates

---
+++ precache-manifest.20443971bb3f58a90eab9ac56e2bbd98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567774aa9e3e0a311d590a135d919323",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5498de4ef1b2fcc498e2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4344a4dbc8d886afde26",
+    "url": "/static/js/2.be4f24de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be4f24de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5498de4ef1b2fcc498e2",
+    "url": "/static/js/main.3fb67151.chunk.js"
+  },
+  {
+    "revision": "538516a55d0e186a764f",
+    "url": "/static/js/runtime-main.447b217d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
